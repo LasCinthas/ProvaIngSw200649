f7aaea6d0507b82f06f945a2662e8aa9de5bff29
Las <mennitieugenio@gmail.com>
2020-11-04 11:50:12 +0100

    Creazione classi principali

---
+++ PrimaProvaIngSw/src/main/java/it/unical/provaingsw/PrimaProvaIngSw/ComparatoreNumeri.java
@@ -0,0 +1,16 @@
+package it.unical.provaingsw.PrimaProvaIngSw;
+
+import java.util.Comparator;
+
+public class ComparatoreNumeri implements Comparator<Integer> {
+
+	@Override
+	public int compare(Integer o1, Integer o2) {
+		if(o1 < o2)
+			return -1;
+		if(o2 < o1)
+			return 1;
+		return 0;
+	}
+
+}
--- PrimaProvaIngSw/src/main/java/it/unical/provaingsw/PrimaProvaIngSw/MyListUtil.java
+++ PrimaProvaIngSw/src/main/java/it/unical/provaingsw/PrimaProvaIngSw/MyListUtil.java
@@ -1,5 +1,38 @@
 package it.unical.provaingsw.PrimaProvaIngSw;
 
-public class MyListUtil {
+import java.util.ArrayList;
+import java.util.Comparator;
 
+public class MyListUtil {
+		
+		ArrayList<Integer> lista = new ArrayList<Integer>();
+		
+		Comparator<Integer> comparatore = new ComparatoreNumeri();
+		Comparator<Integer> comparatoreDec = new ComparatoreNumeriDec();
+		
+		public ArrayList<Integer> ordinaLista(ArrayList<Integer> numeri, String tipoOrdinamento) {
+			if(tipoOrdinamento.equals("crescente")) {
+				numeri.sort(comparatore);
+				return numeri;
+			}
+			if(tipoOrdinamento.equals("decrescente")) {
+				numeri.sort(comparatoreDec);
+				return numeri;
+			}
+			
+			return null;
+		}
+		
+/*		public static void main(String[] args) {
+			ArrayList<Integer> test = new ArrayList<Integer>();
+			test.add(1);
+			test.add(5);
+			test.add(3);
+			MyListUtil ml = new MyListUtil();
+			ml.ordinaLista(test, "decrescente");
+			System.out.println(test.get(0));
+			System.out.println(test.get(1));
+			System.out.println(test.get(2));
+		}
+*/		
 }
